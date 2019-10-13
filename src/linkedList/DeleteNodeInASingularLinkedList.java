b1adc3702a55d58427f9917beb81abd92cdb33b1
Mayank Varma <mayankvarma@MacBook-Pro.local>
2019-10-13 16:51:43 +0530

    DeleteNodeInASingularLinkedList.java

---
+++ src/linkedList/DeleteNodeInASingularLinkedList.java
@@ -0,0 +1,26 @@
+package src.linkedList;
+
+public class DeleteNodeInASingularLinkedList {
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public void deleteNode(ListNode node) {
+        ListNode prev = new ListNode(1);
+        while(node.next != null){
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        
+        prev.next = null;
+    }
+}
+
+/**
+ * Since we only have the reference to a node which is not the head, we don't have access to the rest of the list.
+ * We can do this by copying the value of the next node to the current node. Which gives an impression that we have deleted it.
+ * We are simply copying all values from the right of the node. We then delete the last redundant element. 
+ */
