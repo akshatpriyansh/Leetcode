3b4dafa338cbc0083e58aa674a50bad4acc3d81f
Mayank Varma <mayankvarma@MacBook-Pro.local>
2019-10-13 13:45:38 +0530

    adding a comment in balancedStringSPlit for clarification

--- strings/balancedStringSplit.java
+++ strings/balancedStringSplit.java
@@ -17,3 +17,13 @@ public class balancedStringSplit {
         return ans;
     }
 }
+
+/**
+ * O(n);
+ * This question was poorly worded in the question and can be confused very easily.
+ * For example, if finding maximum balanced strings in RRLRRLLLLRL, we can get RL, RRLL and LR. But the answer is RRLRRLRLLL and RL. 
+ * This is simply counting both characters and increment ans whenever both of their counts match. 
+ *
+ * According to a leetcode user:
+ * Here, we need to count the max number of balanced strings. Balanced strings are the ones having equal number of 'L' and 'R' characters. It does not matter whether they are consecutive or alternate or in any particular order. We just need to find substrings having equal number of 'L'/'R'.
+ */
